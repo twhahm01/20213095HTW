37b8662827db9ee35fbab069c15a8372811519ea
twhahm01 <84127430+twhahm01@users.noreply.github.com>
2021-09-15 10:50:34 +0900

    Update 04_example_3.ino

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -14,15 +14,11 @@ void setup() {
 
 void loop() {
   Serial.println(++count);
-  toggle = 0;
-  digitalWrite(PIN_LED, toggle);
-  delay(100);
   toggle = toggle_state(toggle); //toggle LED value.
-  digitalWrite(PIN_LED, toggle);// update LED status.
+  digitalWrite(PIN_LED, toggle); // update LED status.
   delay(1000); // wait for 1,000 milliseconds
 }
 
 int toggle_state(int toggle) {
-  toggle  = toggle + 1;
-  return toggle;
+  return !toggle;
 }
